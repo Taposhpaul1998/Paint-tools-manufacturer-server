76eb36c51fb20731adeba1c86443f84f0302bfc4
Taposh paul <taposhp05@gamil.com>
2022-05-25 20:43:16 +0600

    loading products in mongodb server

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwtrt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productsCollection = client.db('HeroPaintTools').collection('products');
+
+        // server api 
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = productsCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+        });
+
+    }
+    finally {
+
+    }
+};
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Ranning Hero point tools server');
+});
+
+app.listen(port, () => {
+    console.log('listen port', port);
+});
